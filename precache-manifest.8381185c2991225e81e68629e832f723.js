c27e358fb779ff86f6fde6bc624439a0bffd2c87
Ermogenes Palacio <ermogenes@gmail.com>
2020-07-21 18:24:58 -0300

    Updates

---
+++ precache-manifest.8381185c2991225e81e68629e832f723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1cc19411b260a5938d6810415df2d0",
+    "url": "/reactjs-gh-pages-deploy-test/index.html"
+  },
+  {
+    "revision": "7d7357d8093909881f9a",
+    "url": "/reactjs-gh-pages-deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34797c60b97c8ed6ba65",
+    "url": "/reactjs-gh-pages-deploy-test/static/js/2.00d87e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-gh-pages-deploy-test/static/js/2.00d87e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7357d8093909881f9a",
+    "url": "/reactjs-gh-pages-deploy-test/static/js/main.573cecfd.chunk.js"
+  },
+  {
+    "revision": "4768eb447f42d37d8d35",
+    "url": "/reactjs-gh-pages-deploy-test/static/js/runtime-main.f596ac26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-gh-pages-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
